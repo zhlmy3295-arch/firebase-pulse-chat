2fda9dbfaf1580711836c9d2134524e9424dc736
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-05-27 14:56:13 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    // Allows accessing the dev server from mobile devices on the same network
+    host: true, 
+  }
+});
