10f912671bf4f0a400cb69c78031437405ae855c
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-05-30 00:34:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  base: './'
+  base: '/firebase-pulse-chat/'
 })
