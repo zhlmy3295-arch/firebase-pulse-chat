10ec3788fb19c5441f45d80044ffd3270ac6c27a
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-06-11 17:53:26 +0300

    Update and rename main.tsx to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
 
 const router = createHashRouter([
