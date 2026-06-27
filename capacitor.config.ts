9e2ac993594f1f0dc0bdb1366fda96dc60291370
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-06-27 14:02:10 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'Firebase Pulse Chat',
-  webDir: 'dist'
-};
-
-export default config;
