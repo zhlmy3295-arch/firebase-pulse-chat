3273fcf23512b6b345ebcd7d4d18badd6ae6d992
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-07-23 12:23:08 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,25 @@
+const CACHE_NAME = 'pulse-chat-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// أول ما يتسطب نخزن الملفات
 self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
+
+// أي طلب نجيبه من الكاش الأول
 self.addEventListener('fetch', e => {
-  // فاضي مؤقتا عشان الكاش
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+  );
 });
