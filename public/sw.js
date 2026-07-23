0d056e2ab8a8173e156f2afbe907a4a85719fe49
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-07-23 13:15:55 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,3 @@
-const CACHE_NAME = 'pulse-chat-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-// أول ما يتسطب نخزن الملفات
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-// أي طلب نجيبه من الكاش الأول
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => self.skipWaiting())
+self.addEventListener('activate', e => self.clients.claim())
+self.addEventListener('fetch', e => {})
