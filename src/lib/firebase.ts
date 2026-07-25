8f8722c1d49ffacf82a42e347b6119a8c65e21ec
zhlmy3295-arch <zhlmy3295@gmail.com>
2026-07-25 22:40:34 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,22 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, onValue, serverTimestamp, query, orderByChild, equalTo, remove, set, update, get } from "firebase/database";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-// إعدادات Firebase الخاصة بمشروعك
+// الاعدادات بتاعة مشروعك caat-22ea0
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  projectId: "sat-app2",
-  databaseURL: "https://sat-app2-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyC09jKS84TQQY1Xm9AuveqQxEK60lx5hPc",
+  authDomain: "caat-22ea0.firebaseapp.com",
+  databaseURL: "https://caat-22ea0-default-rtdb.firebaseio.com",
+  projectId: "caat-22ea0",
+  storageBucket: "caat-22ea0.firebasestorage.app",
+  messagingSenderId: "1033524289590",
+  appId: "1:1033524289590:web:66bf11a09f04a5f174f897",
+  measurementId: "G-E8SRRTBQK5"
 };
 
-// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
-
-// تهيئة Realtime Database
 const db = getDatabase(app);
-
-// تهيئة Auth
 const auth = getAuth(app);
 
 export { db, auth, ref, push, onValue, serverTimestamp, query, orderByChild, equalTo, remove, set, update, get, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
-
-
